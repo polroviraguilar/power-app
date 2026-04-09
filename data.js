009a3f1bb3fa6fc3ba3227cd82b38ea08d9cee83
Pol Rovira <proviraguilar@gmail.com>
2026-04-09 10:22:29 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,366 @@
+const progression = {
+
+"Upper 1": {
+
+26:{phase:"Accumulation",exercises:{
+"Bench Press":{top:"Triple @ 115",work:"4x4 @ 107.5"},
+"Paused Bench":{work:"3x4 @ 97.5"},
+"Weighted Pull-ups":{work:"4x6 @ 31.25"},
+"Barbell Row":{work:"4x8 @ 102.5"},
+"Triceps Pushdown":{work:"3x12 @ 35 kg"},
+"Hanging Leg Raises":{work:"3x12"}}},
+
+27:{phase:"Accumulation",exercises:{
+"Bench Press":{top:"Double @ 117.5",work:"4x4 @ 110"},
+"Paused Bench":{work:"3x4 @ 100"},
+"Weighted Pull-ups":{work:"4x6 @ 32.5"},
+"Barbell Row":{work:"4x8 @ 105"},
+"Triceps Pushdown":{work:"3x12 @ 36 kg"},
+"Hanging Leg Raises":{work:"3x12"}}},
+
+28:{phase:"Recruitment",exercises:{
+"Bench Press":{top:"Single @ 120",work:"4x4 @ 112.5"},
+"Paused Bench":{work:"3x4 @ 102.5"},
+"Weighted Pull-ups":{work:"4x6 @ 33.75"},
+"Barbell Row":{work:"4x8 @ 107.5"},
+"Triceps Pushdown":{work:"3x12 @ 37 kg"},
+"Hanging Leg Raises":{work:"3x12"}}},
+
+29:{phase:"DELOAD",exercises:{
+"Bench Press":{top:"-",work:"3x4 @ 100"},
+"Paused Bench":{work:"3x4 @ 85"},
+"Weighted Pull-ups":{work:"3x6 @ 27.5"},
+"Barbell Row":{work:"3x8 @ 85"},
+"Triceps Pushdown":{work:"2x12 @ 30 kg"},
+"Hanging Leg Raises":{work:"2x10"}}},
+
+30:{phase:"Intensification",exercises:{
+"Bench Press":{top:"Triple @ 120",work:"4x4 @ 112.5"},
+"Paused Bench":{work:"3x4 @ 102.5"},
+"Weighted Pull-ups":{work:"4x6 @ 33.75"},
+"Barbell Row":{work:"4x8 @ 107.5"},
+"Triceps Pushdown":{work:"3x12 @ 37 kg"},
+"Hanging Leg Raises":{work:"3x12"}}},
+
+31:{phase:"Intensification",exercises:{
+"Bench Press":{top:"Double @ 122.5",work:"4x4 @ 115"},
+"Paused Bench":{work:"3x4 @ 105"},
+"Weighted Pull-ups":{work:"4x6 @ 35"},
+"Barbell Row":{work:"4x8 @ 110"},
+"Triceps Pushdown":{work:"3x12 @ 38 kg"},
+"Hanging Leg Raises":{work:"3x12"}}},
+
+32:{phase:"Recruitment",exercises:{
+"Bench Press":{top:"Single @ 125",work:"4x4 @ 117.5"},
+"Paused Bench":{work:"3x4 @ 107.5"},
+"Weighted Pull-ups":{work:"4x6 @ 36.25"},
+"Barbell Row":{work:"4x8 @ 112.5"},
+"Triceps Pushdown":{work:"3x12 @ 39 kg"},
+"Hanging Leg Raises":{work:"3x12"}}},
+
+33:{phase:"DELOAD",exercises:{
+"Bench Press":{top:"-",work:"3x4 @ 102.5"},
+"Paused Bench":{work:"3x4 @ 90"},
+"Weighted Pull-ups":{work:"3x6 @ 30"},
+"Barbell Row":{work:"3x8 @ 90"},
+"Triceps Pushdown":{work:"2x12 @ 32 kg"},
+"Hanging Leg Raises":{work:"2x10"}}},
+
+34:{phase:"Intensification",exercises:{
+"Bench Press":{top:"Triple @ 125",work:"4x4 @ 117.5"},
+"Paused Bench":{work:"3x4 @ 107.5"},
+"Weighted Pull-ups":{work:"4x6 @ 36.25"},
+"Barbell Row":{work:"4x8 @ 112.5"},
+"Triceps Pushdown":{work:"3x12 @ 39 kg"},
+"Hanging Leg Raises":{work:"3x12"}}},
+
+35:{phase:"Intensification",exercises:{
+"Bench Press":{top:"Double @ 127.5",work:"4x4 @ 120"},
+"Paused Bench":{work:"3x4 @ 110"},
+"Weighted Pull-ups":{work:"4x6 @ 37.5"},
+"Barbell Row":{work:"4x8 @ 115"},
+"Triceps Pushdown":{work:"3x12 @ 40 kg"},
+"Hanging Leg Raises":{work:"3x12"}}},
+
+36:{phase:"Peaking",exercises:{
+"Bench Press":{top:"Single @ 130",work:"3x3 @ 122.5"},
+"Paused Bench":{work:"3x3 @ 112.5"},
+"Weighted Pull-ups":{work:"4x5 @ 38.75"},
+"Barbell Row":{work:"4x6 @ 117.5"},
+"Triceps Pushdown":{work:"3x10 @ 42 kg"},
+"Hanging Leg Raises":{work:"3x10"}}},
+
+37:{phase:"TEST",exercises:{
+"Bench Press":{top:"Attempt @ 140"}}}
+
+},
+
+"Lower 1": {
+
+26:{phase:"Accumulation",exercises:{
+"Back Squat":{top:"Triple @ 155",work:"4x4 @ 140"},
+"Paused Squat":{work:"3x4 @ 120"},
+"RDL":{work:"3x8 @ 120"},
+"Bulgarian Split Squat":{work:"3x8 @ 60 kg"},
+"Weighted Plank":{work:"3x45s"}}},
+
+27:{phase:"Accumulation",exercises:{
+"Back Squat":{top:"Double @ 160",work:"4x4 @ 145"},
+"Paused Squat":{work:"3x4 @ 122.5"},
+"RDL":{work:"3x8 @ 125"},
+"Bulgarian Split Squat":{work:"3x8 @ 62.5 kg"},
+"Weighted Plank":{work:"3x50s"}}},
+
+28:{phase:"Recruitment",exercises:{
+"Back Squat":{top:"Single @ 165",work:"4x4 @ 150"},
+"Paused Squat":{work:"3x4 @ 125"},
+"RDL":{work:"3x8 @ 130"},
+"Bulgarian Split Squat":{work:"3x8 @ 65 kg"},
+"Weighted Plank":{work:"3x60s"}}},
+
+29:{phase:"DELOAD",exercises:{
+"Back Squat":{top:"-",work:"3x4 @ 125"},
+"Paused Squat":{work:"3x4 @ 105"},
+"RDL":{work:"2x6 @ 105"},
+"Bulgarian Split Squat":{work:"2x8 @ 50 kg"},
+"Weighted Plank":{work:"2x40s"}}},
+
+30:{phase:"Intensification",exercises:{
+"Back Squat":{top:"Triple @ 160",work:"4x4 @ 150"},
+"Paused Squat":{work:"3x4 @ 125"},
+"RDL":{work:"3x8 @ 130"},
+"Bulgarian Split Squat":{work:"3x8 @ 65 kg"},
+"Weighted Plank":{work:"3x45s"}}},
+
+31:{phase:"Intensification",exercises:{
+"Back Squat":{top:"Double @ 165",work:"4x4 @ 155"},
+"Paused Squat":{work:"3x4 @ 127.5"},
+"RDL":{work:"3x8 @ 135"},
+"Bulgarian Split Squat":{work:"3x8 @ 67.5 kg"},
+"Weighted Plank":{work:"3x50s"}}},
+
+32:{phase:"Recruitment",exercises:{
+"Back Squat":{top:"Single @ 170",work:"4x4 @ 160"},
+"Paused Squat":{work:"3x4 @ 130"},
+"RDL":{work:"3x8 @ 140"},
+"Bulgarian Split Squat":{work:"3x8 @ 70 kg"},
+"Weighted Plank":{work:"3x60s"}}},
+
+33:{phase:"DELOAD",exercises:{
+"Back Squat":{top:"-",work:"3x4 @ 135"},
+"Paused Squat":{work:"3x4 @ 110"},
+"RDL":{work:"2x6 @ 115"},
+"Bulgarian Split Squat":{work:"2x8 @ 55 kg"},
+"Weighted Plank":{work:"2x40s"}}},
+
+34:{phase:"Intensification",exercises:{
+"Back Squat":{top:"Triple @ 165",work:"4x4 @ 160"},
+"Paused Squat":{work:"3x4 @ 130"},
+"RDL":{work:"3x8 @ 140"},
+"Bulgarian Split Squat":{work:"3x8 @ 70 kg"},
+"Weighted Plank":{work:"3x45s"}}},
+
+35:{phase:"Intensification",exercises:{
+"Back Squat":{top:"Double @ 170",work:"4x4 @ 165"},
+"Paused Squat":{work:"3x4 @ 132.5"},
+"RDL":{work:"3x8 @ 145"},
+"Bulgarian Split Squat":{work:"3x8 @ 72.5 kg"},
+"Weighted Plank":{work:"3x50s"}}},
+
+36:{phase:"Peaking",exercises:{
+"Back Squat":{top:"Single @ 175",work:"2x3 @ 170"},
+"Paused Squat":{work:"3x3 @ 135"},
+"RDL":{work:"3x6 @ 150"},
+"Bulgarian Split Squat":{work:"3x8 @ 75 kg"},
+"Weighted Plank":{work:"3x60s"}}},
+
+37:{phase:"TEST",exercises:{
+"Back Squat":{top:"Attempt @ 190"}}}
+
+},
+
+"Upper 2": {
+
+26:{phase:"Accumulation",exercises:{
+"Bench Press":{work:"4x6 @ 92.5"},
+"Close Grip Bench":{work:"3x6 @ 87.5"},
+"Overhead Press":{work:"3x8 @ 50 kg"},
+"Chest Supported Row":{work:"4x10 @ 75"},
+"Face Pull":{work:"3x15 @ 30 kg"},
+"Lateral Raises":{work:"3x12 @ 15 kg"}}},
+
+27:{phase:"Accumulation",exercises:{
+"Bench Press":{work:"4x6 @ 95"},
+"Close Grip Bench":{work:"3x6 @ 90"},
+"Overhead Press":{work:"3x8 @ 52.5 kg"},
+"Chest Supported Row":{work:"4x10 @ 77.5"},
+"Face Pull":{work:"3x15 @ 32.5 kg"},
+"Lateral Raises":{work:"3x12 @ 17.5 kg"}}},
+
+28:{phase:"Recruitment",exercises:{
+"Bench Press":{work:"4x6 @ 97.5"},
+"Close Grip Bench":{work:"3x6 @ 92.5"},
+"Overhead Press":{work:"3x8 @ 55 kg"},
+"Chest Supported Row":{work:"4x10 @ 80"},
+"Face Pull":{work:"3x15 @ 35 kg"},
+"Lateral Raises":{work:"3x12 @ 17.5 kg"}}},
+
+29:{phase:"DELOAD",exercises:{
+"Bench Press":{work:"3x6 @ 82.5"},
+"Close Grip Bench":{work:"2x6 @ 75"},
+"Overhead Press":{work:"2x8 @ 45 kg"},
+"Chest Supported Row":{work:"3x10 @ 65"},
+"Face Pull":{work:"2x15 @ 25 kg"},
+"Lateral Raises":{work:"2x12 @ 12.5 kg"}}},
+
+30:{phase:"Intensification",exercises:{
+"Bench Press":{work:"4x6 @ 97.5"},
+"Close Grip Bench":{work:"3x6 @ 92.5"},
+"Overhead Press":{work:"3x8 @ 57.5 kg"},
+"Chest Supported Row":{work:"4x10 @ 80"},
+"Face Pull":{work:"3x15 @ 35 kg"},
+"Lateral Raises":{work:"3x12 @ 17.5 kg"}}},
+
+31:{phase:"Intensification",exercises:{
+"Bench Press":{work:"4x6 @ 100"},
+"Close Grip Bench":{work:"3x6 @ 95"},
+"Overhead Press":{work:"3x8 @ 60 kg"},
+"Chest Supported Row":{work:"4x10 @ 82.5"},
+"Face Pull":{work:"3x15 @ 37.5 kg"},
+"Lateral Raises":{work:"3x12 @ 20 kg"}}},
+
+32:{phase:"Recruitment",exercises:{
+"Bench Press":{work:"4x6 @ 102.5"},
+"Close Grip Bench":{work:"3x6 @ 97.5"},
+"Overhead Press":{work:"3x8 @ 62.5 kg"},
+"Chest Supported Row":{work:"4x10 @ 85"},
+"Face Pull":{work:"3x15 @ 40 kg"},
+"Lateral Raises":{work:"3x12 @ 20 kg"}}},
+
+33:{phase:"DELOAD",exercises:{
+"Bench Press":{work:"3x6 @ 87.5"},
+"Close Grip Bench":{work:"2x6 @ 80"},
+"Overhead Press":{work:"2x8 @ 50 kg"},
+"Chest Supported Row":{work:"3x10 @ 70"},
+"Face Pull":{work:"2x15 @ 27.5 kg"},
+"Lateral Raises":{work:"2x12 @ 15 kg"}}},
+
+34:{phase:"Intensification",exercises:{
+"Bench Press":{work:"4x6 @ 102.5"},
+"Close Grip Bench":{work:"3x6 @ 97.5"},
+"Overhead Press":{work:"3x8 @ 65 kg"},
+"Chest Supported Row":{work:"4x10 @ 85"},
+"Face Pull":{work:"3x15 @ 40 kg"},
+"Lateral Raises":{work:"3x12 @ 20 kg"}}},
+
+35:{phase:"Intensification",exercises:{
+"Bench Press":{work:"4x6 @ 105"},
+"Close Grip Bench":{work:"3x6 @ 100"},
+"Overhead Press":{work:"3x8 @ 67.5 kg"},
+"Chest Supported Row":{work:"4x10 @ 87.5"},
+"Face Pull":{work:"3x15 @ 42.5 kg"},
+"Lateral Raises":{work:"3x12 @ 22.5 kg"}}},
+
+36:{phase:"Peaking",exercises:{
+"Bench Press":{work:"4x4 @ 107.5"},
+"Close Grip Bench":{work:"3x5 @ 102.5"},
+"Overhead Press":{work:"3x6 @ 70 kg"},
+"Chest Supported Row":{work:"4x8 @ 90"},
+"Face Pull":{work:"3x12 @ 45 kg"},
+"Lateral Raises":{work:"3x10 @ 22.5 kg"}}},
+
+37:{phase:"DELOAD",exercises:{
+"Bench Press":{work:"-"},
+"Close Grip Bench":{work:"-"},
+"Overhead Press":{work:"-"},
+"Chest Supported Row":{work:"-"},
+"Face Pull":{work:"-"},
+"Lateral Raises":{work:"-"}}}
+
+},
+
+"Lower 2": {
+
+26:{phase:"Accumulation",exercises:{
+"Deadlift":{top:"Triple @ 160",work:"3x3 @ 150"},
+"Paused Deadlift":{work:"3x3 @ 130"},
+"Zercher's Squat":{work:"3x4 @ 110"},
+"Back Extensions":{work:"3x12 @ ? kg"},
+"Ab Wheel":{work:"3x10"}}},
+
+27:{phase:"Accumulation",exercises:{
+"Deadlift":{top:"Double @ 165",work:"3x3 @ 155"},
+"Paused Deadlift":{work:"3x3 @ 135"},
+"Zercher's Squat":{work:"3x4 @ 112.5"},
+"Back Extensions":{work:"3x12 @ ? kg"},
+"Ab Wheel":{work:"3x10"}}},
+
+28:{phase:"Recruitment",exercises:{
+"Deadlift":{top:"Single @ 170",work:"3x3 @ 160"},
+"Paused Deadlift":{work:"3x3 @ 140"},
+"Zercher's Squat":{work:"3x4 @ 115"},
+"Back Extensions":{work:"3x12 @ ? kg"},
+"Ab Wheel":{work:"3x10"}}},
+
+29:{phase:"DELOAD",exercises:{
+"Deadlift":{top:"-",work:"3x3 @ 135"},
+"Paused Deadlift":{work:"3x3 @ 110"},
+"Zercher's Squat":{work:"2x4 @ 95"},
+"Back Extensions":{work:"3x12 @ ? kg"},
+"Ab Wheel":{work:"2x10"}}},
+
+30:{phase:"Intensification",exercises:{
+"Deadlift":{top:"Triple @ 165",work:"3x3 @ 160"},
+"Paused Deadlift":{work:"3x3 @ 140"},
+"Zercher's Squat":{work:"3x4 @ 115"},
+"Back Extensions":{work:"3x12 @ ? kg"},
+"Ab Wheel":{work:"3x10"}}},
+
+31:{phase:"Intensification",exercises:{
+"Deadlift":{top:"Double @ 170",work:"3x3 @ 165"},
+"Paused Deadlift":{work:"3x3 @ 145"},
+"Zercher's Squat":{work:"3x4 @ 117.5"},
+"Back Extensions":{work:"3x12 @ ? kg"},
+"Ab Wheel":{work:"3x10"}}},
+
+32:{phase:"Recruitment",exercises:{
+"Deadlift":{top:"Single @ 175",work:"3x3 @ 170"},
+"Paused Deadlift":{work:"3x3 @ 150"},
+"Zercher's Squat":{work:"3x4 @ 120"},
+"Back Extensions":{work:"3x12 @ ? kg"},
+"Ab Wheel":{work:"3x10"}}},
+
+33:{phase:"DELOAD",exercises:{
+"Deadlift":{top:"-",work:"3x3 @ 140"},
+"Paused Deadlift":{work:"3x3 @ 115"},
+"Zercher's Squat":{work:"2x4 @ 100"},
+"Back Extensions":{work:"3x12 @ ? kg"},
+"Ab Wheel":{work:"2x10"}}},
+
+34:{phase:"Intensification",exercises:{
+"Deadlift":{top:"Triple @ 170",work:"3x3 @ 170"},
+"Paused Deadlift":{work:"3x3 @ 150"},
+"Zercher's Squat":{work:"3x4 @ 120"},
+"Back Extensions":{work:"3x12 @ ? kg"},
+"Ab Wheel":{work:"3x10"}}},
+
+35:{phase:"Intensification",exercises:{
+"Deadlift":{top:"Double @ 175",work:"3x3 @ 175"},
+"Paused Deadlift":{work:"3x3 @ 155"},
+"Zercher's Squat":{work:"3x4 @ 122.5"},
+"Back Extensions":{work:"3x12 @ ? kg"},
+"Ab Wheel":{work:"3x10"}}},
+
+36:{phase:"Peaking",exercises:{
+"Deadlift":{top:"Single @ 180",work:"2x2 @ 180"},
+"Paused Deadlift":{work:"3x2 @ 160"},
+"Zercher's Squat":{work:"3x3 @ 125"},
+"Back Extensions":{work:"3x12 @ ? kg"},
+"Ab Wheel":{work:"3x8"}}},
+
+37:{phase:"TEST",exercises:{
+"Deadlift":{top:"Attempt @ 200"}}}
+
+}
+
+};
